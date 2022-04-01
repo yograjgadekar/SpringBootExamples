d817c1a0a4fb6a9ace1263b483a94e09e20d38fa
Yograj Gadekar <79786436+yograjgadekar@users.noreply.github.com>
2022-04-01 12:07:50 +0530

    Spring/SpringBoot

---
+++ CoursesWebApp/src/main/WebApp/courses.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Courses Names</title>
+</head>
+<body>
+Top Selling Courses are:
+1. DevOps
+2. Java
+3. Python1
+4. BigData
+5. AWS
+6. SpringBoot1
+7. Angular
+</body>
+</html>
---
+++ CoursesWebApp/src/main/java/com/example/demo/CoursesController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CoursesController {
+	@RequestMapping("/courses")
+	public String courses() {
+		System.out.println("Welcome to the First WebApplication.");
+		return "courses.jsp";
+	}
+
+}
